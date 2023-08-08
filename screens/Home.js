b45b5f1c51e2e08ffc6f7603e06108ad283144d9
unknown <almusallam296@hotmail.com>
2023-08-08 15:36:31 -0700

    Done with Services still. not done with filter by location

--- screens/Home.js
+++ screens/Home.js
@@ -1,22 +1,92 @@
-// screens/Home.js
-import React from "react";
-import { View, StyleSheet } from "react-native";
-
-import { laundries } from "../data/laundriesData"; // Update the path as needed
+import React, { useState } from "react";
+import { View, StyleSheet, ScrollView } from "react-native";
+import { SearchBar } from "react-native-elements";
+import { laundries } from "../data/laundriesData";
 import LaundriesList from "../components/LaundriesList";
 
 const Home = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [locationFilter, setLocationFilter] = useState("");
+  const filteredLaundries = laundries.filter((laundry) => {
+    const nameMatch =
+      laundry.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      searchTerm === "";
+    const locationMatch =
+      laundry.location.toLowerCase().includes(locationFilter.toLowerCase()) ||
+      locationFilter === "";
+    return nameMatch && locationMatch;
+  });
+
   return (
-    <View style={styles.container}>
-      <LaundriesList data={laundries} />
-    </View>
+    <ScrollView style={styles.container}>
+      <View style={styles.searchBarContainer}>
+        <SearchBar
+          placeholder="Search for a laundry"
+          onChangeText={(text) => setSearchTerm(text)}
+          value={searchTerm}
+          inputContainerStyle={styles.searchBarInputContainer}
+          inputStyle={styles.searchBarInput}
+          containerStyle={styles.searchBarMainContainer}
+          placeholderTextColor="#A0A0A0"
+        />
+      </View>
+
+      <View style={styles.searchBarContainer}>
+        <SearchBar
+          placeholder="Filter by location"
+          onChangeText={(text) => setLocationFilter(text)}
+          value={locationFilter}
+          inputContainerStyle={styles.searchBarInputContainer}
+          inputStyle={styles.searchBarInput}
+          containerStyle={styles.searchBarMainContainer}
+          placeholderTextColor="#A0A0A0"
+        />
+      </View>
+
+      <View style={styles.spacer} />
+
+      <View style={styles.listContainer}>
+        <LaundriesList data={filteredLaundries} />
+      </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
+    backgroundColor: "#fff",
+  },
+  searchBarContainer: {
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    margin: 10,
+    elevation: 2,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 2,
+  },
+  searchBarMainContainer: {
+    borderRadius: 10,
+    padding: 0,
+    backgroundColor: "white",
+    borderBottomWidth: 0,
+  },
+  searchBarInputContainer: {
+    backgroundColor: "transparent",
+    borderBottomColor: "transparent",
+    borderTopColor: "transparent",
+  },
+  searchBarInput: {
+    color: "#333",
+    fontSize: 16,
+  },
+  spacer: {
+    height: "20%",
+  },
+  listContainer: {
+    flex: 1,
   },
 });
 
--- screens/Services.js
+++ screens/Services.js
@@ -1,10 +1,128 @@
-import { View, Text } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import { CheckBox } from "react-native-elements"; // Import CheckBox from react-native-elements
 
-export default function Services() {
+const Services = () => {
+  const [services, setServices] = useState({
+    wash: false,
+    dryClean: false,
+    iron: false,
+  });
+
+  const handleServiceToggle = (service) => {
+    setServices((prevServices) => ({
+      ...prevServices,
+      [service]: !prevServices[service],
+    }));
+  };
+
+  const handleAddToBasket = () => {
+    // Handle adding item to basket with selected services
+    console.log("Added to basket:", services);
+  };
+
   return (
-    <View>
-      <Text>Services</Text>
-    </View>
+    <ScrollView contentContainerStyle={styles.container}>
+      <Image source={require("../assets/cloth.png")} style={styles.image} />
+
+      <View style={styles.checklistContainer}>
+        <Text style={styles.heading}>Select Services:</Text>
+        <CheckBox
+          title="Wash"
+          checked={services.wash}
+          onPress={() => handleServiceToggle("wash")}
+          containerStyle={styles.checkboxContainer}
+          textStyle={styles.checkboxText}
+        />
+        <CheckBox
+          title="Dry Clean"
+          checked={services.dryClean}
+          onPress={() => handleServiceToggle("dryClean")}
+          containerStyle={styles.checkboxContainer}
+          textStyle={styles.checkboxText}
+        />
+        <CheckBox
+          title="Iron"
+          checked={services.iron}
+          onPress={() => handleServiceToggle("iron")}
+          containerStyle={styles.checkboxContainer}
+          textStyle={styles.checkboxText}
+        />
+      </View>
+
+      <TouchableOpacity
+        style={[
+          styles.addButton,
+          {
+            opacity:
+              services.wash || services.dryClean || services.iron ? 1 : 0.5,
+          },
+        ]}
+        onPress={handleAddToBasket}
+        disabled={!services.wash && !services.dryClean && !services.iron}
+      >
+        <Text style={styles.addButtonText}>Add to Basket</Text>
+      </TouchableOpacity>
+    </ScrollView>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: 20,
+  },
+  image: {
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "grey",
+    width: 350,
+    height: 350,
+    marginBottom: 20,
+  },
+  checklistContainer: {
+    alignItems: "flex-start",
+    width: "100%",
+  },
+  heading: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 10,
+    alignSelf: "flex-start",
+  },
+  checkboxContainer: {
+    backgroundColor: "transparent",
+    borderWidth: 0,
+    padding: 0,
+    marginLeft: 0,
+    marginRight: 0,
+    alignSelf: "flex-start",
+  },
+  checkboxText: {
+    fontSize: 16,
+  },
+  addButton: {
+    backgroundColor: "#007AFF",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    marginTop: 20,
+    alignSelf: "flex-end",
+  },
+  addButtonText: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+});
+
+export default Services;
