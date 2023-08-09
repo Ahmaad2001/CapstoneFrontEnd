df721c7006e9aaeb10e1a02f626354b6f0c7d52f
Abdullah Fahad <iamabdullahkw@gmail.com>
2023-08-09 23:49:04 +0300

    Fix merge with fix

--- screens/Home.js
+++ screens/Home.js
@@ -1,8 +1,8 @@
-import React from "react";
-import { View, StyleSheet } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-
 // import { laundries } from "../data/laundriesData";
+import React, { useState } from "react";
+import { View, StyleSheet, ScrollView } from "react-native";
+import { SearchBar } from "react-native-elements";
 import LaundriesList from "../components/LaundriesList";
 import { useQuery } from "@tanstack/react-query";
 import { getAllLaundries } from "../api/laundries";
@@ -16,18 +16,90 @@ const Home = () => {
   const handleLaundryPress = (id, laundry) => {
     navigation.navigate("LaundryDetails", { id, laundry });
   };
+
+  const [searchTerm, setSearchTerm] = useState("");
+  const [locationFilter, setLocationFilter] = useState("");
+  const filteredLaundries = laundries?.filter((laundry) => {
+    const nameMatch =
+      laundry.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      searchTerm === "";
+    const locationMatch =
+      laundry.location.toLowerCase().includes(locationFilter.toLowerCase()) ||
+      locationFilter === "";
+    return nameMatch && locationMatch;
+  });
+
   if (!laundries) return null;
   return (
-    <View style={styles.container}>
-      <LaundriesList data={laundries} onPress={handleLaundryPress} />
-    </View>
+    <ScrollView style={styles.container}>
+      <View style={styles.searchBarContainer}>
+        <SearchBar
+          placeholder="Search for a laundry"
+          onChangeText={(text) => setSearchTerm(text)}
+          value={searchTerm}
+          inputContainerStyle={styles.searchBarInputContainer}
+          inputStyle={styles.searchBarInput}
+          containerStyle={styles.searchBarMainContainer}
+          placeholderTextColor="#A0A0A0"
+        />
+      </View>
+
+      <View style={styles.searchBarContainer}>
+        <SearchBar
+          placeholder="Filter by location"
+          onChangeText={(text) => setLocationFilter(text)}
+          value={locationFilter}
+          inputContainerStyle={styles.searchBarInputContainer}
+          inputStyle={styles.searchBarInput}
+          containerStyle={styles.searchBarMainContainer}
+          placeholderTextColor="#A0A0A0"
+        />
+      </View>
+
+      <View style={styles.spacer} />
+
+      <View style={styles.container}>
+        <LaundriesList data={filteredLaundries} onPress={handleLaundryPress} />
+      </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
+    backgroundColor: "#fff",
+  },
+  searchBarContainer: {
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    margin: 10,
+    elevation: 2,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 2,
+  },
+  searchBarMainContainer: {
+    borderRadius: 10,
+    padding: 0,
+    backgroundColor: "white",
+    borderBottomWidth: 0,
+  },
+  searchBarInputContainer: {
+    backgroundColor: "transparent",
+    borderBottomColor: "transparent",
+    borderTopColor: "transparent",
+  },
+  searchBarInput: {
+    color: "#333",
+    fontSize: 16,
+  },
+  spacer: {
+    height: "20%",
+  },
+  listContainer: {
+    flex: 1,
   },
 });
 
